f268ef1056093d5b442a7069c7ea162ab7d6271e
MGaracciolo <mgaracciolo@gmail.com>
2023-11-05 17:32:49 -0300

    se modifican funcionalidades y estilos de muestra

--- myapp/routes/rutaMuestra.js
+++ myapp/routes/rutaMuestra.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {muestra} = require('../models');
+const {tipo_muestra} = require('../models');
 
 router.post('/agregar', async (req, res) => {
     const { 
@@ -32,5 +33,27 @@ router.post('/agregar', async (req, res) => {
       });
     
   }});
+
+  router.get('/buscarMuestra', async (req, res) => {
+    try {
+      const tipos_muestra = await tipo_muestra.findAll({
+        attributes: ['id_tipo_muestra', 'valor','descripcion'],
+      });
+      if (tipos_muestra.length === 0) {
+        res.render('/rutaMuestra/agregarr', {
+          noResult: true
+        });
+      } else {
+        res.render('/rutaMuestra/agregar', {
+          tipos_muestra
+        });
+      }
+    } catch (error) {
+      console.error('Error al buscar los tipos de muestra:', error);
+      res.status(500).send('Error al buscar los tipos de muestra');
+    }
+  });
+ 
+     
 
 module.exports = router;
